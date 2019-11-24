0ce8674bc155bb6dce8fc0be9d9ff2d36259b59b
petrero <petrero@gmail.com>
2019-11-24 12:45:46 +0200

    35.4.Custom Validator{Requiring Owner}
    To fix testCreateCheeseListing previous commit error, above owner, add the missing @Assert\NotBlank().
    
    The value must now not be blank and it must be a valid owner. Try the test again:
    
    php bin/phpunit --filter=testCreateCheeseListing
    
    It's green! Next, allowing owner to be part of our API is great for admin users. But it's kind of inconvenient for normal users. Could we allow the owner field to be sent when creating a CheeseListing... but automatically set it to the currently-authenticated user if the field is not sent? And if so, how? That's next.

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -103,6 +103,7 @@ class CheeseListing {
 	 * @ORM\JoinColumn(nullable=false)
 	 * @Groups({"cheese:read", "cheese:collection:post"})
 	 * @IsValidOwner()
+	 * @Assert\NotBlank()
 	 */
 	private $owner;
 
